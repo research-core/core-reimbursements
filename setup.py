fce73ad31a0962f2c9c865a4967d4bf5ba69ee4a
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-08-09 17:11:38 +0100

    Update docs

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
-import os
-from setuptools import find_packages, setup
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fd:
+    long_description = fd.read()
 
 setup(
     name='core-reimbursements',
-    version=1.0,
-    description='Core purchases reimbursements',
-    author=[
-        'Hugo Cachitas',
-        'Ricardo Ribeiro',
-    ],
-    author_email=[
-        'hugo.cachitas@research.fchampalimaud.org',
-        'ricardojvr@gmail.com',
-    ],
+    version='0.0',
+    description='Research CORE ERM - reimbursements module',
+    author='Ricardo Ribeiro, Hugo Cachitas',
+    author_email='ricardojvr@gmail.com, hugo.cachitas@research.fchampalimaud.org',
+    url='https://github.com/research-core/core-reimbursements',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
-    include_package_data=True,
-    requirements=[
-        'localflavor'
-    ]
+    license='CC BY-NC 2.0',
 )
